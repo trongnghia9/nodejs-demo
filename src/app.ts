2aa2b153ca270929b4efeb9cfceb238cda64fcc7
Harry <107847294+trongnghia9@users.noreply.github.com>
2025-05-31 09:11:07 +0700

    add

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 const app = express();
+require('dotenv').config()||8080;
 const port = 8080;
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World ád');
 });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:8080 : ${port}`);
+  console.log("env port:", process.env.PORT);
 });
